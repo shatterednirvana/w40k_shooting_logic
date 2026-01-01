9243a2361bfa00da5a5a0ab589159d921027c38e
Chris Bunch <shatterednirvana@gmail.com>
2026-01-01 07:50:26 -0800

    added pseudocode for project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    /*
+     * ok, here's the basic idea:
+     *
+     * loop {
+     *   generate strength (random number between 2 and 12)
+     *   generate toughness (random number between 2 and 12)
+     *   figure out what the correct min dice roll would be from this
+     *   ask the user what they think
+     *   compare them
+     * }
+     *
+     */
+
     println!("Hello, world!");
 }
